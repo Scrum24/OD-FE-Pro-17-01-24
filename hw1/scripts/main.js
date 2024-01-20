a73d1f462504e530cc1dbe71e679eee6f62eb490
Scrum24 <orlyanskiy.dmitry@gmail.com>
2024-01-20 17:02:23 +0100

    Lesson 1. Homework 1

---
+++ hw1/scripts/main.js
@@ -0,0 +1,5 @@
+const initialMessage = 'Hi. Please, enter your name. Thank you!';
+const enteredName = window.prompt(initialMessage);
+
+const helloMessage = `Hello, ${enteredName}! How are you?`;
+window.alert(helloMessage);
