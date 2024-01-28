63a2da9bc3513bdd530db831c1c7502368f99e1d
Scrum24 <orlyanskiy.dmitry@gmail.com>
2024-01-28 02:44:35 +0100

    Lesson 4. Homework 8. Refactoring.

--- hw8/scripts/main.js
+++ hw8/scripts/main.js
@@ -10,42 +10,40 @@ console.log("Case_1 |", arr1.toString());
 const price = 27;
 const arr2 = [];
 for (let i = 10; i <= 100; i = i + 10) {
-    const volume = i * price;
-    arr2.push(volume);
+    arr2.push(i * price);
 }
 console.log("Case_2 |", arr2.toString());
 
 
 //case_3: the square of which <= the number N
-const arr3 = [7, 25, 38, 50, 76];
-arr3.forEach( testNumber => {
+const testArr3 = [7, 25, 38, 50, 76];
+testArr3.forEach( numb => {
 
     const arr3 = [];
     for (let i = 1; i <= 100; i++) {
-        if (Math.pow(i, 2) <= testNumber){
+        if (Math.pow(i, 2) <= numb){
             arr3.push(i);
         } else {
             break;
         }
     }
-    console.log("Case_3 |", `testNumber = ${testNumber}, arr3 = ${arr3.toString()}.`);
-
+    console.log("Case_3 |", `testNumber = ${numb}, arr3 = ${arr3.toString()}.`);
 });
 
 
 //case_4: Prime numbers 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47...
-const arr4 = [2, 3, 4, 5, 6, 7];
-arr4.forEach( testNumber => {
-    const isNumbPrime = isPrime(testNumber);
-    console.log("Case_4 |",`testNumber = ${testNumber}, isNumbPrime = ${isNumbPrime}.`);
+const testArr4 = [2, 3, 4, 5, 6, 7];
+testArr4.forEach( numb => {
+    const isNumbPrime = isPrime(numb);
+    console.log("Case_4 |",`testNumber = ${numb}, isNumbPrime = ${isNumbPrime}.`);
 });
 
-function isPrime(testNumber){
-    if (testNumber < 2){
+function isPrime(numb){
+    if (numb < 2){
         return false;
     }
-    for (let i = 2; i < testNumber; i++) {
-        if ( testNumber % i === 0){
+    for (let i = 2; i < numb; i++) {
+        if ( numb % i === 0){
             return false;
         }
     }
@@ -54,23 +52,23 @@ function isPrime(testNumber){
 
 
 //case_5: E.g.: 4 or 100 (false), 9 or 81 (true)
-const arr5 = [4, 9, 81, 100];
-arr5.forEach( testNumber => {
-    const canReceiveNumb = canReceiveNumber(testNumber);
-    console.log("Case_5 |",`testNumber = ${testNumber}, canReceiveNumb = ${canReceiveNumb}.`);
+const testArr5 = [4, 9, 81, 100];
+testArr5.forEach( numb => {
+    const canReceiveNumb = canReceiveNumber(numb);
+    console.log("Case_5 |",`testNumber = ${numb}, canReceiveNumb = ${canReceiveNumb}.`);
 });
 
-function canReceiveNumber(testNumber){
+function canReceiveNumber(numb){
     let degree = 1;
     let root;
 
     do {
         root = Math.pow(3, degree);
-        if (root === testNumber){
+        if (root === numb){
             return true;
         }
         degree++;
-    } while (root <= testNumber);
+    } while (root <= numb);
 
     return false;
 }
