e5186be57f4819a13b7c2bd8b7c3dcc12cc8b774
Scrum24 <orlyanskiy.dmitry@gmail.com>
2024-03-08 01:41:57 +0100

    Homework 23. Refactoring

--- hw23/scripts/main.js
+++ hw23/scripts/main.js
@@ -4,8 +4,9 @@ main.addEventListener('focusin', isActiveDiv);
 main.addEventListener('focusout', isActiveDiv);
 
 function isActiveDiv(e){
-    if (e.target.id === "testInput"){
-        const divEl = document.getElementById('testDiv');
+    const listeningEl = "testInput";
+    if (e.target.id === listeningEl){
+        const divEl = document.querySelector(`[data-main-el=${listeningEl}]`);
         divEl.classList.toggle("isActive");
     }
 }
