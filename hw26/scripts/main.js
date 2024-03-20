be64dfdb5b6230e96273a93c46f9403d0b28cb79
Scrum24 <orlyanskiy.dmitry@gmail.com>
2024-03-20 23:34:34 +0100

    Homework 28. Added homework.

--- hw26/scripts/main.js
+++ hw26/scripts/main.js
@@ -119,7 +119,6 @@ const phones = [
 ];
 
 
-
 //nav block
 const nav = document.getElementById("nav");
 nav.addEventListener("click", collectsProductsPage);
@@ -233,23 +232,20 @@ function buyProduct(e) {
         const prodId = cardElDataset.dataset.prodId;
 
         const product = getProductBy(productCategory, prodId);
-        const buyMessage = `Hello. Congratulations! \n You bought "${product.title}" (price: ${product.price})`;
-
-        alert(buyMessage);
-        closeProductBlocks();
+        actualizeProductBuyForm(product.title, product.price);
     }
 }
 
-function closeProductBlocks() {
-    setTimeout(() => {
-        closeProductCard();
-    }, 1000);
+function actualizeProductBuyForm(productTitle, productPrice) {
+    const formProdTitle = document.getElementById("form-prod-title");
+    formProdTitle.innerText = productTitle;
 
-    setTimeout(() => {
-        closeProducts();
-    }, 2000);
+    const formProdPrice = document.getElementById("form-prod-price");
+    formProdPrice.innerText = productPrice;
+
+    const bayFormMidWind = document.getElementById("buy-form-modal-w");
+    bayFormMidWind.classList.remove("isHideModal");
 }
-
 
 //cleaning block
 const products = document.getElementById("products");
@@ -286,3 +282,83 @@ function closeProducts() {
         .forEach(product => product.remove());
     productsBlock.classList.add("isHide");
 }
+
+
+
+// form block
+const submitBtn = document.getElementById("buy-form");
+submitBtn.addEventListener("submit", buyProductForm);
+
+function buyProductForm(e) {
+    e.preventDefault();
+    const form = e.target.closest("form");
+    const formInfo = createObjFrom(form);
+    addOrderBlock(formInfo);
+
+    hideBuyProductForm();
+    closeProductCard();
+    closeProducts();
+}
+
+function createObjFrom(form) {
+    const formProdTitle = document.getElementById("form-prod-title").innerText;
+    const formProdPrice = document.getElementById("form-prod-price").innerText;
+
+    const firstName = form.elements['fullName'].value;
+    const email = form.elements['email'].value;
+    const phone = form.elements['phone'].value;
+    const city = form.elements['city'].value;
+    const warehouse = form.elements['warehouse'].value;
+    const payment = form.elements['payment'].value;
+    const prodCount = form.elements['prodCount'].value;
+    const comment = form.elements['comment'].value;
+
+    return {formProdTitle, formProdPrice, firstName, email, phone, city, warehouse, payment, prodCount, comment};
+}
+
+function addOrderBlock(content) {
+    const orderBlock = `<div class="order">
+        <div class="close-btn">x</div>
+        <div class="order__title">Order</div>
+        <div class="order__content">
+            <b>Product</b>: ${content.formProdTitle}</br>
+            <b>Price</b>: ${content.formProdPrice}</br>
+            
+            <b>Full name</b>: ${content.firstName}</br>
+            <b>Email</b>: ${content.email}</br>
+            <b>Phone</b>: ${content.phone}</br>
+            <b>City</b>: ${content.city}</br>
+            <b>NP warehouse</b>: ${content.warehouse}</br>
+            <b>Payment method</b>: ${content.payment}</br>
+            <b>Products count</b>: ${content.prodCount}</br>
+            <b>Comment</b>: ${content.comment}
+        </div>
+    </div>`;
+
+    const aside = document.getElementById("aside");
+    aside.insertAdjacentHTML("beforeend", orderBlock);
+}
+
+const formCloseBtn = document.querySelector("form > div.close-btn");
+formCloseBtn.addEventListener("click", () => {
+    hideBuyProductForm();
+});
+
+function hideBuyProductForm() {
+    const bayFormMidWind = document.getElementById("buy-form-modal-w");
+    bayFormMidWind.classList.add("isHideModal");
+}
+
+
+// aside
+const aside = document.getElementById("aside");
+aside.addEventListener("click", removeOrderInfoBlock);
+
+function removeOrderInfoBlock(e) {
+    const clickedEl = e.target;
+
+    if (clickedEl.className === "close-btn") {
+        const order = clickedEl.closest(".order");
+        order.remove();
+    }
+};
